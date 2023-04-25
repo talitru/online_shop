b0af9711d81a0611cfd6c4a4dfe1d76de940f1d0
talitru <talitru123@gmail.com>
2023-04-25 14:26:04 +0300

    converting the layout to handlebars

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ const port = 3000;
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const expressHbs = require('express-handlebars');
 
-app.set('view engine', 'pug'); // set()- any values globally on our express application.  
-app.set('views', 'views'); //we tellin express that we want to compile dynamic templates with the pug engine and where to find these templates 
+app.engine('hbs', expressHbs()); //registers a new templating engine in case we using one which is not build in like pug
+app.set('view engine', 'hbs'); // set()- any values globally on our express application.  
+app.set('views', 'views'); //we tellin express that we want to compile dynamic templates with the pug/handlebars engine and where to find these templates 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
