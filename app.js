194b039ff00d53e15d059de1edee032d9e766659
talitru <talitru123@gmail.com>
2023-04-26 20:16:22 +0300

    working with handlebars engine(before was with pug engine)

--- app.js
+++ app.js
@@ -5,18 +5,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const expressHbs = require('express-handlebars');
 
-app.engine('hbs', expressHbs()); //registers a new templating engine in case we using one which is not build in like pug
+app.engine('hbs', expressHbs({layoutDir: 'views/layouts/', defaultLayout: 'main-layout', extname:'hbs'})); //registers a new templating engine in case we using one which is not build in like pug
 app.set('view engine', 'hbs'); // set()- any values globally on our express application.  
 app.set('views', 'views'); //we tellin express that we want to compile dynamic templates with the pug/handlebars engine and where to find these templates 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
