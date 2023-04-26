9688d7dec2daa564674a4d57716923b1b4d092d0
talitru <talitru123@gmail.com>
2023-04-26 21:28:45 +0300

    working with ejs

--- app.js
+++ app.js
@@ -7,10 +7,8 @@ const port = 3000;
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const expressHbs = require('express-handlebars');
 
-app.engine('hbs', expressHbs({layoutDir: 'views/layouts/', defaultLayout: 'main-layout', extname:'hbs'})); //registers a new templating engine in case we using one which is not build in like pug
-app.set('view engine', 'hbs'); // set()- any values globally on our express application.  
+app.set('view engine', 'ejs'); // set()- any values globally on our express application.  
 app.set('views', 'views'); //we tellin express that we want to compile dynamic templates with the pug/handlebars engine and where to find these templates 
 
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/shop.js
+++ routes/shop.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const express = require('express');
 
-const rootDir = require('../util/path');
 const adminData = require('./admin');
 
 const router = express.Router();
